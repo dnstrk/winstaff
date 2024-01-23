5a30b89dbfffc7afb33de69492853a31cb7f7d8b
dnstrk <stfuidkama@gmail.com>
2024-01-23 17:30:18 +0300

    animation, paralax

--- src/App.jsx
+++ src/App.jsx
@@ -2,89 +2,182 @@ import { useEffect, useState } from "react";
 import "./App.scss";
 import Header from "./components/Header/Header";
 import TownOverlay from "./components/TownOverlay/TownOverlay";
+import Typed from "typed.js";
 
 function App() {
-  const [currentTown, setCurrentTown] = useState("Москва");
-  const [townSearchValue, setTownSearchValue] = useState("");
-  const [townOverlay, setTownOverlay] = useState(false);
+    const [currentTown, setCurrentTown] = useState("Москва");
+    const [townSearchValue, setTownSearchValue] = useState("");
+    const [townOverlay, setTownOverlay] = useState(false);
 
-  const [phone, setPhone] = useState("");
-  const [mail, setMail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [mail, setMail] = useState("");
 
-  useEffect(() => {
-    console.log(document.body.classList.toggle('disable'));
-  }, [townOverlay]);
+    useEffect(() => {
+        console.log(document.body.classList.toggle("disable"));
+    }, [townOverlay]);
 
-  const towns = [
-    "Абакан",
-    "Барнаул",
-    "Волгоград",
-    "Геленджик",
-    "Дзержинск",
-    "Елабуга",
-    "Екатеринбург",
-    "Железнодорожный",
-    "Москва",
-    "Санкт-Петербург",
-  ];
+    useEffect(() => {
+        const s5 = document.querySelector(".s5");
+        const s8 = document.querySelector(".s8");
+        document.onmousemove = (e) => {
+            let x = e.clientX / window.innerWidth;
+            let y = e.clientY / window.innerHeight;
 
-  return (
-    <div className="App">
-      <Header currentTown={currentTown} setTownOverlay={setTownOverlay} />
-      <TownOverlay
-        currentTown={currentTown}
-        setCurrentTown={setCurrentTown}
-        towns={towns}
-        townOverlay={townOverlay}
-        setTownOverlay={setTownOverlay}
-        townSearchValue={townSearchValue}
-        setTownSearchValue={setTownSearchValue}
-      />
-      <section className="section__banner">
-        <div className="container">
-          <div className="banner-wrap">
-            <div className="banner__info">
-              <h2 className="banner__infoTitle">
-                <span className="banner__infoTitle_marked">
-                  Поиск персонала
-                </span>
-                <br />
-                на короткий срок
-              </h2>
-              <p className="banner__infoText">
-                Лидер на рынке аутсорсинга временного персонала. <br /> Более{" "}
-                <b>54 отделений</b> в нашей региональной сети.
-              </p>
-              <div className="banner__actionInp">
-                <input
-                  className="banner__actionInp__num"
-                  type="text"
-                  value={phone}
-                  onChange={(e) => setPhone(e.target.value)}
-                  placeholder="Ваш телефон"
-                />
-                <input
-                  className="banner__actionInp__email"
-                  type="text"
-                  value={mail}
-                  onChange={(e) => setMail(e.target.value)}
-                  placeholder="Ваш e-mail"
-                />
-              </div>
-            </div>
-            <img
-              className="banner__img"
-              width={653}
-              height={630}
-              src="/img/bannerPerson.svg"
-              alt=""
+            s5.style.transform =
+                "translate(-" + x * 20 + "px, -" + y * 20 + "px)";
+            s8.style.transform =
+                "translate(+" + x * 20 + "px, +" + y * 20 + "px)";
+        };
+    });
+
+    useEffect(() => {
+        var typed = new Typed(".s3_text", {
+            strings: ["Более 300 000 <br /> анкет в базе"],
+            typeSpeed: 60,
+            // backSpeed: 100,
+            // backDelay: 5000,
+            // fadeOut: true,
+            // loop: true,
+        });
+    });
+
+    const towns = [
+        "Абакан",
+        "Барнаул",
+        "Волгоград",
+        "Геленджик",
+        "Дзержинск",
+        "Елабуга",
+        "Екатеринбург",
+        "Железнодорожный",
+        "Москва",
+        "Санкт-Петербург",
+    ];
+
+    return (
+        <div className="App">
+            <Header currentTown={currentTown} setTownOverlay={setTownOverlay} />
+            <TownOverlay
+                currentTown={currentTown}
+                setCurrentTown={setCurrentTown}
+                towns={towns}
+                townOverlay={townOverlay}
+                setTownOverlay={setTownOverlay}
+                townSearchValue={townSearchValue}
+                setTownSearchValue={setTownSearchValue}
             />
-          </div>
+            <section className="section__banner">
+                <div className="container">
+                    <div className="banner-wrap">
+                        <div className="banner__info">
+                            <h2 className="banner__infoTitle">
+                                <span className="banner__infoTitle_marked">
+                                    Поиск персонала
+                                </span>
+                                <br />
+                                на короткий срок
+                            </h2>
+                            <p className="banner__infoText">
+                                Лидер на рынке аутсорсинга временного персонала.{" "}
+                                <br /> Более <b>54 отделений</b> в нашей
+                                региональной сети.
+                            </p>
+                            <form action="" className="banner__form">
+                                <div className="banner__actionInp">
+                                    <input
+                                        className="banner__actionInp__num"
+                                        type="text"
+                                        value={phone}
+                                        onChange={(e) =>
+                                            setPhone(e.target.value)
+                                        }
+                                        placeholder="Ваш телефон"
+                                    />
+                                    <input
+                                        className="banner__actionInp__email"
+                                        type="text"
+                                        value={mail}
+                                        onChange={(e) =>
+                                            setMail(e.target.value)
+                                        }
+                                        placeholder="Ваш e-mail"
+                                    />
+                                </div>
+                                <p className="banner__specText">
+                                    Выберите специалиста
+                                </p>
+                                <div className="banner__specRadioGroup">
+                                    <div className="banner__specRadioGroup__btn">
+                                        <label className="banner__specRadioGroup__btnLbl">
+                                            <input
+                                                className="banner__specRadioGroup__btnInp"
+                                                type="radio"
+                                            />
+                                            Кассир
+                                        </label>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="banner__img">
+                            <img
+                                className="banner__imgAbs s1"
+                                src="/img/bannerImgMain.png"
+                                alt=""
+                            />
+                            <img
+                                className="banner__imgAbs s2"
+                                src="/img/bannerOrangeEllipse.svg"
+                                alt=""
+                            />
+                            <img
+                                className="banner__imgAbs s3"
+                                src="/img/bannerForm1.svg"
+                                alt=""
+                            />
+                            <img
+                                className="banner__imgAbs s3_1"
+                                src="/img/bannerForm1dop.svg"
+                                alt=""
+                            />
+                            <b className="banner__imgAbs s3_text"></b>
+                            <img
+                                className="banner__imgAbs s4"
+                                src="/img/bannerForm2.svg"
+                                alt=""
+                            />
+                            <img
+                                className="banner__imgAbs s5"
+                                src="/img/bannerLightOrangeEllipse.svg"
+                                alt=""
+                            />
+                            <img
+                                className="banner__imgAbs s6"
+                                src="/img/bannerHorDots.svg"
+                                alt=""
+                            />
+                            <img
+                                className="banner__imgAbs s7"
+                                src="/img/bannerVertDots.svg"
+                                alt=""
+                            />
+                            <img
+                                className="banner__imgAbs s8"
+                                src="/img/bannerGreenEllipse.svg"
+                                alt=""
+                            />
+                            <img
+                                className="banner__imgAbs s9"
+                                src="/img/bannerHorLines.svg"
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <div className="test">a</div>
         </div>
-      </section>
-      <div className="test">a</div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/components/TownOverlay/TownOverlay.jsx
+++ src/components/TownOverlay/TownOverlay.jsx
@@ -2,74 +2,85 @@ import React from "react";
 import cl from "./TownOverlay.module.scss";
 
 const TownOverlay = ({
-  currentTown,
-  setCurrentTown,
-  towns,
-  townSearchValue,
-  setTownSearchValue,
-  townOverlay,
-  setTownOverlay,
+    currentTown,
+    setCurrentTown,
+    towns,
+    townSearchValue,
+    setTownSearchValue,
+    townOverlay,
+    setTownOverlay,
 }) => {
-  const closeOverlay = (e) => {
-    e.target.classList.contains(cl.overlay) && setTownOverlay(false);
-  };
+    const closeOverlay = (e) => {
+        e.target.classList.contains(cl.overlay) && setTownOverlay(false);
+    };
 
-  return (
-    <div
-      onClick={(e) => closeOverlay(e)}
-      className={`${cl.overlay} ${townOverlay && cl.overlay__visible}`}
-    >
-      <div className={cl.town__select}>
-        <div className={cl.town__selectHead}>
-          <span className={cl.town__selectHead__text}>Ваш город:</span>
-          <span className={cl.town__selectHead__currentTown}>
-            {currentTown}
-          </span>
-          <button
-            onClick={(e) => setTownOverlay(false)}
-            className={cl.town__selectHead__btn}
-          >
-            <img
-              className={cl.town__selectHead__btnImg}
-              width={20}
-              height={20}
-              src="/img/closeBtn.svg"
-              alt=""
-            />
-          </button>
-        </div>
-        <div className={cl.town__selectSearch}>
-          <input
-            className={cl.town__selectSearch__input}
-            placeholder="Выбрать город..."
-            type="text"
-            value={townSearchValue}
-            onChange={(e) => setTownSearchValue(e.target.value)}
-          />
-          <button className={cl.town__selectSearch__btn}>Найти</button>
-        </div>
-        <div className={cl.town__selectTowns}>
-          {towns.map((town, index) =>
-            town.toLowerCase().includes(townSearchValue.toLowerCase()) &&
-            townSearchValue.toLowerCase() != "" ? (
-              <b
-                onClick={(e) => setCurrentTown(town)}
-                className={cl.town__selectTown}
-                key={index}
-              >
-                {town}
-              </b>
-            ) : (
-              <p
-                onClick={(e) => setCurrentTown(town)}
-                className={cl.town__selectTown}
-                key={index}
-              >
-                {town}
-              </p>
-            )
-          )}
-          {/* {towns
+    const setTownAndQuit = (town) => {
+        setCurrentTown(town);
+        setTownOverlay(false);
+    };
+
+    return (
+        <div
+            onClick={(e) => closeOverlay(e)}
+            className={`${cl.overlay} ${townOverlay && cl.overlay__visible}`}
+        >
+            <div className={cl.town__select}>
+                <div className={cl.town__selectHead}>
+                    <span className={cl.town__selectHead__text}>
+                        Ваш город:
+                    </span>
+                    <span className={cl.town__selectHead__currentTown}>
+                        {currentTown}
+                    </span>
+                    <button
+                        onClick={(e) => setTownOverlay(false)}
+                        className={cl.town__selectHead__btn}
+                    >
+                        <img
+                            className={cl.town__selectHead__btnImg}
+                            width={20}
+                            height={20}
+                            src="/img/closeBtn.svg"
+                            alt=""
+                        />
+                    </button>
+                </div>
+                <div className={cl.town__selectSearch}>
+                    <input
+                        className={cl.town__selectSearch__input}
+                        placeholder="Выбрать город..."
+                        type="text"
+                        value={townSearchValue}
+                        onChange={(e) => setTownSearchValue(e.target.value)}
+                    />
+                    <button className={cl.town__selectSearch__btn}>
+                        Найти
+                    </button>
+                </div>
+                <div className={cl.town__selectTowns}>
+                    {towns.map((town, index) =>
+                        town
+                            .toLowerCase()
+                            .includes(townSearchValue.toLowerCase()) &&
+                        townSearchValue.toLowerCase() != "" ? (
+                            <b
+                                onClick={(e) => setTownAndQuit(town)}
+                                className={cl.town__selectTown}
+                                key={index}
+                            >
+                                {town}
+                            </b>
+                        ) : (
+                            <p
+                                onClick={(e) => setTownAndQuit(town)}
+                                className={cl.town__selectTown}
+                                key={index}
+                            >
+                                {town}
+                            </p>
+                        )
+                    )}
+                    {/* {towns
             .filter((town) =>
               town.toLowerCase().includes(townSearchValue.toLowerCase())
             )
@@ -82,10 +93,10 @@ const TownOverlay = ({
                 {town}
               </p>
             ))} */}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default TownOverlay;
